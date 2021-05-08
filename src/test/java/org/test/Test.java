3433b287ad983b5e831f272d97c94f661e39b7f9
Muhajir M.R <mohamemr@amazon.com>
2021-05-08 10:02:46 -0700

    test

---
+++ src/test/java/org/test/Test.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class Test {
+	public static void main(String[] args) {
+		int a = 10;
+		if (a <= 10) {
+			System.out.println("true");
+		} else {
+			System.out.println("false");
+		}
+		
+		
+}
+}
